b1f9486a4a95f696bd92035b94bfc1445ed36810
AptetutBen <admin@aptetut.com>
2024-01-16 08:44:21 +1100

    Added UI particles

--- Assets/Scripts/Network Demo/EmotePopup.cs
+++ Assets/Scripts/Network Demo/EmotePopup.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Coffee.UIExtensions;
 
 public class EmotePopup : MonoBehaviour
 {
-	[SerializeField] Image image;
+	[SerializeField] private UIParticle particles;
 
 	public void Initialise(Sprite sprite)
 	{
-		image.sprite = sprite;
+		particles.material.SetColor("_Color", Color.red);
+
+		//particles.material.SetTexture("_MainTex",sprite.texture);
 		Destroy(gameObject, 2);
 	}
 }
